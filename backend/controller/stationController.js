eba871aa658732a1ade06755cb6492be7f197421
Sayyed-12 <103912729+Sayyed-12@users.noreply.github.com>
2025-07-05 18:06:31 +0530

    Added station module

---
+++ backend/controller/stationController.js
@@ -0,0 +1,85 @@
+import mongoose from "mongoose";
+import Station from "../models/stationModel.js";
+
+//create a station
+export const createStation = async (req, res) => {
+  try {
+    const { name, code } = req.body;
+
+    const existing = await Station.findOne({ $or: [({ name }, { code })] });
+    if (existing) {
+      res.status(400).json({ message: "Station already exists" });
+    }
+    const station = await Station.create({ name, code });
+    res.status(201).json(station);
+  } catch (err) {
+    res.status(500), json({ message: err.message });
+  }
+};
+
+//get all stations
+export const getStations = async (req, res) => {
+  try {
+    const stations = await Station.find();
+    res.json(stations);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//get single station by id
+
+export const getStationById = async (req, res) => {
+  try {
+    //validate mongodb object id (as per mongodb id format)
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: "invalid sattion id" });
+    }
+    //get station id from url
+    const station = await Station.findById(req.params.id);
+
+    //if the station with req id isnt found
+    if (!station) {
+      res.status(404).json({ message: "Station with this id is not found" });
+    }
+    //if found ,return the station
+    res.json(station);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//update a station
+export const updateStation = async (req, res) => {
+  try {
+    const { name, code } = req.body;
+    const station = await Station.findByIdAndUpdate(
+      //it returns the updated document,after searching the id from params and puting re.body data there
+      req.params.id,
+      { name, code },
+      { new: true }
+    );
+    if (!station) {
+      //if findByIdAndUpdate returned null(due to user provided id not found in Station)
+      res.status(404).json({ message: "Station not found" });
+    }
+    res.json(station);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//delete a station
+export const deleteStation = async (req, res) => {
+  try {
+    const station = await Station.findByIdAndDelete(req.params.id);
+    if (!station) {
+      res.status(404).json({ message: "Station Not Found or already deleted" });
+    }
+    res.json({ message: "Station deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,24 @@
-import express from 'express';
-import connectDB from './config/db.js';
-import { errorHandler } from './middleware/errorHandler.js';
-import TrainRoute from './routes/trainRoutes.js';
-import authRoutes from "./routes/authRoutes.js"
+import express from "express";
+import connectDB from "./config/db.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import TrainRoute from "./routes/trainRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import stationRoutes from "./routes/stationRoutes.js";
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/trains', TrainRoute);
-app.use('/api', authRoutes);
+app.use("/trains", TrainRoute);
+app.use("/api", authRoutes);
+app.use("/api/stations", stationRoutes);
 
-app.get('/', (req, res) => {
-  res.json({ message: 'root API hit, Hi' });
+app.get("/", (req, res) => {
+  res.json({ message: "root API hit, Hi" });
 });
 
 app.use((req, res, next) => {
-  const error = new Error('Page not found');
+  const error = new Error("Page not found");
   error.status = 404;
   return next(error);
 });
@@ -26,5 +29,3 @@ app.listen(process.env.PORT, () => {
   connectDB();
   console.log(`server is listening on port ${process.env.PORT}`);
 });
-
-
